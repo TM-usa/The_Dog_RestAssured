0a5190d89a430bb1b900080631d631e0d8596137
Thapelo Musa <89559442+ThapeloMusa@users.noreply.github.com>
2023-03-16 14:58:30 +0200

    commit

--- src/test/java/categories/getTest.java
+++
@@ -1,7 +0,0 @@
-package categories;
-
-public class getTest {
-//    @DisplayName("getting category by sub Id.")
-//    @Test
-
-}
---
+++ src/test/java/tests/getTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import config.DogsConfig;
+
+public class getTest extends DogsConfig {
+//    @DisplayName("getting category")
+//    @Test
+    public void getAllCategories(){
+
+    }
+
+
+}
